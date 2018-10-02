1ceb28ba7ee81cf0ea9d033374932ef9720c5956
laerton2016 <laerton240311@gmail.com>
2018-10-01 22:12:33 -0300

    Processo de inicialização de ativaidade.

---
+++ server-rest/src/main/java/br/edu/ifpb/server/rest/entidades/Livro.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.server.rest.entidades;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author laerton
+ */
+@Entity
+public class Livro implements Serializable {
+   @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+   private String edicao;
+   private String descricao;
+   private String titulo;
+   private List<Autor> autores;
+   @OneToMany
+   private List<Reserva> reservas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(List<Reserva> reservas) {
+        this.reservas = reservas;
+    }
+   
+   
+   
+}
---
+++ server-rest/src/main/java/br/edu/ifpb/server/rest/entidades/Reserva.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.server.rest.entidades;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author laerton
+ */
+@Entity
+public class Reserva implements Serializable{
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String data;
+    @OneToOne
+    private Livro livro;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+    
+    
+}
